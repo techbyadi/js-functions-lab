d7ded38e6c0ca691ff83d9817c7eaaeff6d30262
techbyadi <pssharma.aditya@gmail.com>
2024-07-10 18:46:37 -0700

    completed exercise #12

--- script.js
+++ script.js
@@ -252,3 +252,17 @@ const createUsername = (firstName, lastName) => {
 
 console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
 
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+const numArgs = (...args) => {
+  return args.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1,2,3));
